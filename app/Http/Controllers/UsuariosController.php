7c478ba11159335bacab2d4dfbb532620d170c92
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-03-31 08:16:55 -0300

    refactor: altera getUsuario para retornar somente o nome, total de pontos e a posição, também altera updateUsuario

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\UserService;
-use Illuminate\Http\Request;
-use Tymon\JWTAuth\Facades\JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
-
-use App\Models\Usuario;
 use App\Models\Log;
-
+use App\Models\Usuario;
+use App\Services\UserService;
 use Carbon\Carbon;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UsuariosController extends Controller
 {
@@ -21,7 +20,7 @@ class UsuariosController extends Controller
         $this->userService = $userService;
     }
 
-   public function login(Request $request)
+    public function login(Request $request)
     {
         $this->validate($request, [
             'email' => 'required|email',
@@ -31,7 +30,7 @@ class UsuariosController extends Controller
             'email.email' => 'Por favor, insira um e-mail válido.',
             'password.required' => 'Por favor, insira sua senha.',
         ]);
-        
+
         $usuario = Usuario::query()
             ->where([
                 'email' => $request->email,
@@ -69,54 +68,70 @@ class UsuariosController extends Controller
         ]);
     }
 
-    public function getUsuario() {
-        $usuario = auth()->user()->load([
-            'participante.destinos' => function ($query) {
-                $query->where('ano_vigente', 2024);
-            },
-            'participante.passaporte' => function ($query) {
-                $query->where('excluido', NULL);
-            },
-            'participante.destinos' => function ($query) {
-                $query->where('excluido', NULL);
-            },
-        ]);
+    public function getUsuario()
+    {
+        $participanteAutenticado = auth()->user();
+
+        $ranking = Usuario::query()
+            ->where([
+                'excluido' => NULL,
+                'funcao' => 'participante',
+            ])
+            ->with([
+                'participante' => function ($q) {
+                    $q->where('excluido', NULL)
+                        ->with(['pontos' => function ($query) {
+                            $query->where('excluido', NULL);
+                        }]);
+                }
+            ])
+            ->get()
+            ->map(function ($usuario) {
+                $pontos = $usuario->participante->pontos ?? collect();
+
+                $total = $pontos->sum(function ($ponto) {
+                    return $ponto->tipo === 'adicao'
+                        ? $ponto->quantidade
+                        : -$ponto->quantidade;
+                });
+
+                return [
+                    'id' => $usuario->id,
+                    'pontos' => $total,
+                ];
+            })
+            ->sortByDesc('pontos')
+            ->values();
+
+        $posicao = null;
+        $totalPontos = 0;
+
+        foreach ($ranking as $index => $item) {
+            if ($item['id'] == $participanteAutenticado->id) {
+                $posicao = $index + 1;
+                break;
+            }
+        }
+
+        $pontos = $participanteAutenticado->participante->pontos ?? collect();
+        $totalPontos = $pontos->sum(function ($ponto) {
+            return $ponto->tipo === 'adicao'
+                ? $ponto->quantidade
+                : -$ponto->quantidade;
+        });
 
         $formattedUsuario = [
-            'nome_completo' => $usuario->participante->nome_completo ?? '',
-            'cpf' => $usuario->participante->cpf ? sprintf('%s%s%s.%s%s%s.%s%s%s-%s%s', ...str_split($usuario->participante->cpf)) : '',
-            'data_nascimento' => $usuario->participante->data_nascimento ?? '',
-            'email' => $usuario->email ?? '',
-            'rg' => $usuario->participante->rg ?? '',
-            'data_expedicao_rg' => $usuario->participante->data_expedicao_rg ?? '',
-            'numero' => $usuario->participante->passaporte->numero ?? '',
-            'paginas_em_branco' => $usuario->participante->passaporte->paginas_em_branco ?? '',
-            'data_emissao' => $usuario->participante->passaporte->data_emissao ?? '',
-            'data_validade' => $usuario->participante->passaporte->data_validade ?? '',
-            // 'fone_fixo' => $usuario->participante->fone_fixo ?? '',
-            // 'fone_comercial' => $usuario->participante->fone_comercial ?? '',
-            'fone_celular' => $usuario->participante->fone_celular ?? '',
-            'fone_emergencia' => $usuario->participante->fone_emergencia ?? '',
-            'aprovado_bloqueado' => $usuario->participante->aprovado_bloqueado ? true : false,
-            'primeiro_acesso' => $usuario->participante->primeiro_acesso ? true : false,
-            'restricao_alimentar' => $usuario->participante->restricao_alimentar ? true : false,
-            'restricao_alimentar_qual' => $usuario->participante->restricao_alimentar ? $usuario->participante->restricao_alimentar_qual : NULL,
-            'limitacao' => $usuario->participante->limitacao ? true : false,
-            'limitacao_qual' => $usuario->participante->limitacao ? $usuario->participante->limitacao_qual : NULL,
-            'medicamento' => $usuario->participante->medicamento ? true : false,
-            'medicamento_qual' => $usuario->participante->medicamento ? $usuario->participante->medicamento_qual : NULL,
-            'medicamento_dosagem' => $usuario->participante->medicamento ? $usuario->participante->medicamento_dosagem : NULL,
-            'problema_saude' => $usuario->participante->problema_saude ? true : false,
-            'problema_saude_qual' => $usuario->participante->problema_saude ? $usuario->participante->problema_saude_qual : NULL,
-            'destinos' => $usuario->participante->destinos->map(function ($destino) {
-                return $destino->id;
-            })->toArray(),
+            'nome' => $participanteAutenticado->nome ?? '',
+            'total_pontos' => $totalPontos ?? '',
+            'posicao' => $posicao ?? '',
+
         ];
 
         return response()->json($formattedUsuario);
     }
 
-    public function setPrimeiroAcesso(Request $request) {
+    public function setPrimeiroAcesso(Request $request)
+    {
         $usuario = auth()->user()->load([
             'participante.destinos' => function ($query) {
                 $query->where('ano_vigente', 2024);
@@ -137,102 +152,88 @@ class UsuariosController extends Controller
         }
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
         auth()->logout();
         return response()->json(['message' => 'Logout realizado com sucesso.']);
     }
 
-    public function updateUsuario(Request $request) {
-        $participanteId = auth()->user()->participante->id;
-        $usuarioId = auth()->user()->id;
+    public function updateUsuario(Request $request)
+    {
+        $usuario = auth()->user();
+
+        $participanteId = $usuario->participante->id;
 
         $this->validate($request, [
-            'nome_completo' => 'required|string|max:255',
-            'email' => 'required|email|unique:usuarios,email,' . $usuarioId,
-            'cpf' => 'required|cpf|unique:cadastros_participantes,cpf,' . $participanteId,
-            'data_nascimento' => 'required|date_format:Y-m-d|before_or_equal:today',
             'rg' => 'required|string|max:20',
-            'data_expedicao_rg' => 'required|date_format:Y-m-d|before_or_equal:today',
+            'data_expedicao_rg' => 'required|date_format:d/m/Y',
+            'cpf' => 'required|cpf|unique:cadastros_participantes,cpf,' . $participanteId,
+            'data_nascimento' => 'required|date_format:d/m/Y',
             'fone_celular' => 'required|celular_com_ddd',
-            // 'fone_fixo' => 'nullable|telefone_com_ddd',
-            // 'fone_comercial' => 'required|celular_com_ddd',
             'fone_emergencia' => 'required|celular_com_ddd',
-            'destinos' => 'required|array|min:1',
-            'destinos.*' => 'exists:destinos,id',
             'restricao_alimentar' => 'required|boolean',
-            'restricao_alimentar_qual' => 'nullable|max:120',
+            'restricao_alimentar_qual' => 'nullable|max:72',
             'limitacao' => 'required|boolean',
-            'limitacao_qual' => 'nullable|max:120',
+            'limitacao_qual' => 'nullable|max:72',
             'medicamento' => 'required|boolean',
-            'medicamento_qual' => 'nullable|max:120',
+            'medicamento_qual' => 'nullable|max:72',
+            'medicamento_dosagem' => 'nullable|max:24',
             'problema_saude' => 'required|boolean',
-            'problema_saude_qual' => 'nullable|max:120',
-            'paginas_em_branco' => 'required|integer|digits_between:1,2',
-            'numero' => 'required|max:30',
-            'data_emissao' => 'required|date_format:Y-m-d|before_or_equal:today',
-            'data_validade' => 'required|date_format:Y-m-d',
+            'problema_saude_qual' => 'nullable|max:72',
+            'nome' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:usuarios,email,' . $usuario->id,
+            'password' => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required',
         ], [
-            'nome_completo.required' => 'Por favor, informe seu nome completo.',
-            'email.required' => 'Por favor, informe seu e-mail.',
-            'email.email' => 'Por favor, informe um e-mail válido.',
-            'email.unique' => 'Este e-mail já está registrado no programa.',
-            'cpf.required' => 'Por favor, informe seu CPF.',
-            'cpf.cpf' => 'Por favor, informe um CPF válido.',
-            'cpf.unique' => 'Este CPF já está registrado no programa.',
-            'data_nascimento.required' => 'Por favor, informe sua data de nascimento.',
-            'data_nascimento.date' => 'Por favor, informe uma data de nascimento válida.',
-            'data_nascimento.before_or_equal' => 'A data de nascimento não pode ser uma data futura.',
             'rg.required' => 'Por favor, informe seu RG.',
             'data_expedicao_rg.required' => 'Por favor, informe a data de expedição do seu RG.',
             'data_expedicao_rg.date' => 'Por favor, informe uma data válida para a expedição do RG.',
-            'data_expedicao_rg.before_or_equal' => 'A data de expedição do RG não pode ser uma data futura.',
+            'data_nascimento.required' => 'Por favor, informe sua data de nascimento.',
+            'data_nascimento.date' => 'Por favor, informe uma data de nascimento válida.',
             'fone_celular.required' => 'Por favor, informe seu telefone.',
             'fone_celular.celular_com_ddd' => 'Por favor, informe um telefone válido.',
-            // 'fone_fixo.telefone_com_ddd' => 'Por favor, informe um telefone fixo válido.',
-            // 'fone_comercial.required' => 'Por favor, informe seu telefone comercial.',
-            // 'fone_comercial.celular_com_ddd' => 'Por favor, informe um telefone comercial válido.',
+            'fone_fixo.telefone_com_ddd' => 'Por favor, informe um telefone fixo válido.',
+            'fone_comercial.required' => 'Por favor, informe seu telefone comercial.',
+            'fone_comercial.celular_com_ddd' => 'Por favor, informe um telefone comercial válido.',
             'fone_emergencia.required' => 'Por favor, informe um contato para emergências.',
             'fone_emergencia.celular_com_ddd' => 'Por favor, informe um contato para emergências válido.',
-            'destinos.required' => 'Por favor, selecione pelo menos um destino.',
-            'destinos.array' => 'Ocorreu um erro ao informar os destinos, atualize a página.',
-            'destinos.min' => 'Por favor, selecione pelo menos um destino.',
-            'destinos.*.exists' => 'Um ou mais destinos informados são inválidos.',
             'restricao_alimentar.required' => 'Por favor, informe se há restrição alimentar.',
             'restricao_alimentar.boolean' => 'Valor inválido para restrição alimentar, atualize a página.',
-            'restricao_alimentar_qual.max' => 'A restrição alimentar deve ter no máximo 120 caracteres.',
+            'restricao_alimentar_qual.max' => 'A restrição alimentar deve ter no máximo 72 caracteres.',
             'limitacao.required' => 'Por favor, informe se há limitação.',
             'limitacao.boolean' => 'Valor inválido para limitação, atualize a página.',
-            'limitacao_qual.max' => 'A limitação deve ter no máximo 120 caracteres.',
+            'limitacao_qual.max' => 'A limitação deve ter no máximo 72 caracteres.',
             'medicamento.required' => 'Por favor, informe se há medicamento.',
             'medicamento.boolean' => 'Valor inválido para medicamento, atualize a página.',
-            'medicamento_qual.max' => 'O medicamento deve ter no máximo 120 caracteres.',
+            'medicamento_qual.max' => 'O medicamento deve ter no máximo 72 caracteres.', //Criar validacao para dosagem
+            'medicamento_dosagem.max' => 'A dosagem do medicamento deve ter no máximo 24 caracteres.',
             'problema_saude.required' => 'Por favor, informe se há problema de saúde.',
             'problema_saude.boolean' => 'Valor inválido para problema de saúde, atualize a página.',
-            'problema_saude_qual.max' => 'O problema de saúde deve ter no máximo 120 caracteres.',
-            'paginas_em_branco.required' => 'Por favor, informe a quantidade de páginas em branco.',
-            'paginas_em_branco.integer' => 'O valor de páginas em branco deve estar em formato de número.',
-            'paginas_em_branco.digits_between' => 'O número de páginas em branco deve ter no máximo 2 caracteres.',
-            'numero.required' => 'Por favor, informe o número do passaporte.',
-            'numero.max' => 'O número do passaporte deve ter no máximo 30 caracteres.',
-            'data_emissao.required' => 'Por favor, informe a emissão do passaporte.',
-            'data_emissao.date_format' => 'A data de emissão do passaporte deve estar no formato d/m/Y.',
-            'data_emissao.before_or_equal' => 'A data de emissão do passaporte não pode ser uma data futura.',
-            'data_validade.required' => 'Por favor, informe a validade do passaporte.',
-            'data_validade.date_format' => 'A data de validade do passaporte deve estar no formato d/m/Y.',
+            'problema_saude_qual.max' => 'O problema de saúde deve ter no máximo 72 caracteres.',
+            'nome.required' => 'Por favor, informe seu nome.',
+            'email.required' => 'Por favor, informe seu e-mail.',
+            'email.email' => 'Por favor, informe um e-mail válido.',
+            'email.unique' => 'Este e-mail já está registrado no programa.',
+            'password.required' => 'Por favor, informe sua senha.',
+            'password.min' => 'A senha deve ter no mínimo 6 caracteres.',
+            'password.confirmed' => 'As senhas não conferem.',
+            'password_confirmation.required' => 'Por favor, confirme sua senha.',
+            'cpf.required' => 'Por favor, informe seu CPF.',
+            'cpf.cpf' => 'Por favor, informe um CPF válido.',
+            'cpf.unique' => 'Este CPF já está registrado no programa.',
         ]);
 
-        $dadosUsuario = $request->only(['email']);
-        $dadosParticipante = $request->only(['nome_completo', 'cpf', 'data_nascimento', 'rg', 'data_expedicao_rg', 'fone_celular', 'fone_emergencia', 'destinos', 'restricao_alimentar', 'restricao_alimentar_qual', 'limitacao', 'limitacao_qual', 'medicamento', 'medicamento_qual', 'medicamento_dosagem', 'problema_saude', 'problema_saude_qual']);
-        $dadosPassaporte = $request->only(['numero', 'paginas_em_branco', 'data_emissao', 'data_validade']);
+        $dadosUsuario = $request->only(['nome', 'email', 'password']);
+        $dadosParticipante = $request->only(['cpf', 'data_nascimento', 'rg', 'data_expedicao_rg', 'fone_celular', 'fone_emergencia', 'restricao_alimentar', 'restricao_alimentar_qual', 'limitacao', 'limitacao_qual', 'medicamento', 'medicamento_qual', 'medicamento_dosagem', 'problema_saude', 'problema_saude_qual']);
 
         try {
-            $response = $this->userService->atualizarCadastro($dadosUsuario, $dadosParticipante, $dadosPassaporte);
+            $response = $this->userService->atualizarCadastro($usuario, $dadosUsuario, $dadosParticipante);
 
             return response()->json([
                 'success' => true,
                 'data' => $response,
                 'message' => 'Cadastro atualizado com sucesso.'
-            ], 201);
+            ], 200);
         } catch (QueryException $e) {
             return response()->json([
                 'success' => false,
