109d7c4c35c79fa0efc03e265e2d80a9034577a3
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-03-31 08:12:00 -0300

    refactor: adiciona import de Controller

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Usuario;
 use App\Services\StepsService;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 
