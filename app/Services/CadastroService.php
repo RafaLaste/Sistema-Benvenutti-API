ea3cb264944305d3fbedbb829f324ab172befab5
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-01 10:02:16 -0300

    refactor: altera service de StepsService para CadastroService

--- app/Services/CadastroService.php
+++ app/Services/CadastroService.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class StepsService
+class CadastroService
 {
     public function completarCadastro($usuario, $dadosUsuario, $dadosParticipante)
     {
