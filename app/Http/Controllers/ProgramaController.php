f1cf8cc41fb86041e93b68349a144edb2ad2983f
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-07 15:51:10 -0300

    feat: adiciona ProgramaController para participantes

---
+++ app/Http/Controllers/ProgramaController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\Programa;
+use Illuminate\Support\Carbon;
+
+class ProgramaController extends Controller
+{
+    public function __invoke()
+    {
+        $programa = Programa::where('id', 1)->first();
+
+        if (!$programa) {
+            return response()->json([
+                'error' => 'Programa não encontrado.'
+            ], 404);
+        }
+
+        $cadastrosAtivos = Carbon::now()->between(
+            Carbon::parse($programa->data_inicio),
+            Carbon::parse($programa->data_final)
+        );
+
+        return response()->json([
+            'programa' => [
+                'titulo' => $programa->titulo,
+                'descricao' => $programa->descricao,
+                'data_inicio' => $programa->data_inicio,
+                'data_final' => $programa->data_final,
+                'regulamento' => $programa->regulamento
+                    ? config('services.site.storage') . '/content/files/' . $programa->regulamento
+                    : null,
+            ],
+            'cadastros_ativos' => $cadastrosAtivos
+        ]);
+    }
+}
